1abe0d40990f537b82ba4eaced4a866afc07a6cf
xiaojin666 <407958149@qq.com>
2026-04-18 21:55:42 +0800

    修改了路径

--- src/train.py
+++ src/train.py
@@ -89,14 +89,17 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
 
     if cfg.get("test"):
         log.info("Starting testing!")
-        ckpt_path = trainer.checkpoint_callback.best_model_path
-        if ckpt_path == "":
-            log.warning(
-                "Best ckpt not found! Using current weights for testing..."
-            )
-            ckpt_path = None
+        # Use user-provided checkpoint path if available
+        ckpt_path = cfg.get("ckpt_path")
+        if not ckpt_path:
+            ckpt_path = trainer.checkpoint_callback.best_model_path
+            if ckpt_path == "":
+                log.warning(
+                    "Best ckpt not found! Using current weights for testing..."
+                )
+                ckpt_path = None
         trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
-        log.info(f"Best ckpt path: {ckpt_path}")
+        log.info(f"Test ckpt path: {ckpt_path}")
 
     test_metrics = trainer.callback_metrics
 
